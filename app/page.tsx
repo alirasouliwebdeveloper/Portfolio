a0b20b5d7ccf62b5a2966b20a1ed2e03c67af0c3
alirasouliwebdeveloper <ali.rasouli.webdeveloper@gmail.com>
2024-02-18 16:08:42 +0330

    Navbar started
    in video 19:22

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Nav from "./Components/Nav";
 
 function homePage() {
-  return <div>homePage</div>;
+  return (
+    <div className="overflow-x-hidden">
+      <div>
+        <Nav />
+      </div>
+    </div>
+  );
 }
 
 export default homePage;
