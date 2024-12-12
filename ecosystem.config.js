ee01b023ffd57248810eded7562c0cbbdf72e7ab
Ali Rasouli <75220661+alirasouliwebdeveloper@users.noreply.github.com>
2024-12-12 18:52:36 +0330

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,24 @@
 module.exports = {
-  apps: [
-    {
-      name: "Portfolio",
-      script: "npm",
-      args: "start",
-      env: {
-        PORT: 3000, // تغییر به پورت دلخواه
-        NODE_ENV: "production",
-      },
-    },
-  ],
+    apps: [
+        {
+            name: "main", // محیط اصلی (Production)
+            script: "npm",
+            args: "start",
+            cwd: "/var/www/main", // مسیر برنچ main
+            env: {
+                NODE_ENV: "production",
+                PORT: 80,
+            },
+        },
+        {
+            name: "develop", // محیط توسعه (Development)
+            script: "npm",
+            args: "start",
+            cwd: "/var/www/develop", // مسیر برنچ feature/develop
+            env: {
+                NODE_ENV: "development",
+                PORT: 3000,
+            },
+        },
+    ],
 };
